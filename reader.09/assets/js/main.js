7e8fb549aa6783c9d911b8398670c1610a1031f1
bste0009 <88520742+bste0009@users.noreply.github.com>
2021-11-09 13:37:36 +1100

    Deploy

---
+++ reader.09/assets/js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    console.log("ready");
+  
+    $(".button").click(function () {
+      $(".bigflatnow").toggleClass("open");
+    });
+  });
+
+
+
+  $(document).ready(function () {
+    console.log("ready");
+    
+  $(".buttontwo").click(function () {
+    $(".overcoming").toggleClass("open");
+  });
+});
+
+
+
+  $(document).ready(function () {
+  console.log("ready");
+  
+  $(".buttonthree").click(function () {
+    $(".rave").toggleClass("open");
+  });
+});
+
+
+$(document).ready(function () {
+  console.log("ready");
+  
+  $(".buttonfour").click(function () {
+    $(".everything").toggleClass("open");
+  });
+});
+
+
